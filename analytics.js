3c42cb19554d558e40a0e29ad44c2ef40c412fe6
PokeChina Wholesale <usma.pokemon.2026@gmail.com>
2026-07-22 11:53:48 -0700

    Fix Meta Pixel initialization

--- analytics.js
+++ analytics.js
@@ -13,23 +13,28 @@
   function gtag() {
     window.dataLayer.push(arguments);
   }
+  window.gtag = window.gtag || gtag;
 
   gtag("js", new Date());
   gtag("config", measurementId);
 
   if (metaPixelId && metaPixelId !== "META_PIXEL_ID_HERE") {
-    window.fbq = window.fbq || function () {
-      (window.fbq.q = window.fbq.q || []).push(arguments);
-    };
-    window._fbq = window.fbq;
-    window.fbq.loaded = true;
-    window.fbq.version = "2.0";
-    window.fbq.queue = [];
-
-    var metaScript = document.createElement("script");
-    metaScript.async = true;
-    metaScript.src = "https://connect.facebook.net/en_US/fbevents.js";
-    document.head.appendChild(metaScript);
+    (function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = true;
+      n.version = "2.0";
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = true;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
 
     window.fbq("init", metaPixelId);
     window.fbq("track", "PageView");
